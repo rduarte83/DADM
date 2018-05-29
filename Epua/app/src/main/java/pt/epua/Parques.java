f9ce3aa43edfc8754d5cb069835c9b00f0fe4c6e
Rui Duarte <r3v3nant@gmail.com>
2018-05-29 04:57:53 +0100

    Revert "dialog numberpicker"
    This reverts commit 21b1c1a4bc16656384634f47c3a104180e3e1d1d.

--- Epua/app/src/main/java/pt/epua/Parques.java
+++ Epua/app/src/main/java/pt/epua/Parques.java
@@ -101,10 +101,6 @@ public class Parques extends AppCompatActivity {
 
         RecyclerView recyclerView = findViewById(R.id.rv);
 
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        recyclerView.setHasFixedSize(true);
-
         mAdapter = new AdapterParques(parqueList);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(mLayoutManager);
